e40f22f1df9e6c7236a587b1aa4e465ff3ec93dd
mm <mmmdev2003@gmail.com>
2025-10-31 11:14:31 +0300

    init

--- internal/config/config.py
+++ internal/config/config.py
@@ -34,4 +34,4 @@ class Config:
         self.loom_authorization_port = int(os.getenv("LOOM_AUTHORIZATION_PORT", "8081"))
 
         self.loki_host = os.getenv("LOOM_LOKI_CONTAINER_NAME", "localhost")
-        self.loki_port = int(os.getenv("LOOM_LOKI_PORT", "8081"))
+        self.loki_port = int(os.getenv("LOOM_LOKI_HTTP_PORT", "3100"))
